fb3b91c46c7516ef8e251c865f1217f11f36331b
Mouiad JRA <mouiad.alilo@gmail.com>
2022-05-29 19:42:31 +0300

    WIP: add date for each transaction

--- script.js
+++ script.js
@@ -4,9 +4,22 @@
 // Data
 const account1 = {
   owner: "Mouiad Gyahd Ali",
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, //
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
+  interestRate: 1.2, // %
   pin: 1111,
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
 };
 
 const account2 = {
@@ -14,6 +27,19 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const account3 = {
@@ -21,6 +47,19 @@ const account3 = {
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const account4 = {
@@ -28,6 +67,19 @@ const account4 = {
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
 const accounts = [account1, account2, account3, account4];
@@ -115,7 +167,9 @@ const openModel = function (modal) {
 const closeModal = function (modal) {
   modal.classList.add("hidden");
 };
-
+const now = new Date();
+labelDate.textContent = `${now.getDate()}`.padStart(2,0) +`/` + `${now.getMonth()+1}`.padStart(2,0)+`/
+ ${now.getFullYear()}, ${now.getHours()} :`+`${now.getMinutes()}`.padStart(2,0);
 // overlay.addEventListener("click", closeModal);
 btnCloseModel.addEventListener("click", closeModal.bind(null, welcomeModal));
 btnTransferCloseModel.addEventListener(
@@ -151,18 +205,22 @@ btnCloseWrongPinAccountModal.addEventListener(
   closeModal.bind(null, accountCloseWrongPinModal)
 );
 
-const displayMovement = function (movement, sort = false) {
+const displayMovement = function (acc, sort = false) {
   containerMovements.innerHTML = "";
-  const movs = sort ? movement.slice().sort((a, b) => a - b) : movement;
+  const movs = sort ? acc.movements.slice().sort((a, b) => a - b) : acc.movements;
   movs.forEach(function (mov, i) {
     const type = mov > 0 ? "deposit" : "withdrawal";
+
+    const date = new Date(acc.movementsDates[i])
+    const displayDate = `${date.getDate()}`.padStart(2,0) +`/` + `${date.getMonth()+1}`.padStart(2,0)+`/
+ ${date.getFullYear()}, ${date.getHours()}:`+`${date.getMinutes()}`.padStart(2,0);
     const html = `
             <div class="movements__row">
             <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-            <div class="movements__date">Null</div>
-            <div class="movements__value">${mov} €</div>
+            <div class="movements__date">${displayDate}</div>
+            <div class="movements__value">${mov.toFixed(2)} €</div>
         </div>`;
     containerMovements.insertAdjacentHTML("afterbegin", html);
   });
@@ -181,27 +239,28 @@ createUsernames(accounts);
 
 const calcDisplayBalance = function (account) {
   account.balance = account.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${account.balance} € `;
+  labelBalance.textContent = `${account.balance.toFixed(2)} € `;
 };
 
 const calcDisplaySummary = function (account) {
   const incomes = account.movements
     .filter((mov) => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes} €  `;
+  labelSumIn.textContent = `${incomes.toFixed(2)} €  `;
   const out = account.movements
     .filter((mov) => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)} € `;
-  labelSumInterest.textContent = `${account.movements
-    .filter((mov) => mov > 0)
-    .map((deposit) => (deposit * account.interestRate) / 100)
-    .filter((int) => int >= 1)
-    .reduce((acc, int) => acc + int, 0)}  €  `;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)} € `;
+  const interest = account.movements
+      .filter((mov) => mov > 0)
+      .map((deposit) => (deposit * account.interestRate) / 100)
+      .filter((int) => int >= 1)
+      .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest.toFixed(2)}  €  `;
 };
 
 const updateUI = function (acc) {
-  displayMovement(acc.movements);
+  displayMovement(acc);
   calcDisplayBalance(acc);
   calcDisplaySummary(acc);
 };
@@ -258,6 +317,8 @@ btnTransfer.addEventListener("click", function (event) {
     // Transfer the Money
     currentAccount.movements.push(-amount);
     receiverAccount.movements.push(amount);
+    currentAccount.movementsDates.push(new Date().toISOString());
+    receiverAccount.movementsDates.push(new Date().toISOString());
     // update Ui
     updateUI(currentAccount);
   }
@@ -275,7 +336,7 @@ btnLoan.addEventListener("click", function (event) {
     openModelWithOutOverlay(requestNegModal);
     return;
   }
-  const amount = Number(inputLoanAmount.value);
+  const amount = Math.floor(Number(inputLoanAmount.value));
   inputLoanAmount.value = "";
   if (
     amount > 0 &&
@@ -285,6 +346,7 @@ btnLoan.addEventListener("click", function (event) {
     // TODO : fix the loan logic , when you request n,
     //  and have it you can request more and more, fix that
     currentAccount.movements.push(amount);
+    currentAccount.movementsDates.push(new Date().toISOString());
     updateUI(currentAccount);
   }
 });
@@ -330,7 +392,7 @@ btnClose.addEventListener("click", function (event) {
 let sorted = false;
 btnSort.addEventListener("click", function (event) {
   event.preventDefault();
-  displayMovement(currentAccount.movements, !sorted);
+  displayMovement(currentAccount, !sorted);
   sorted = !sorted;
 });
 /////////////////////////////////////////////////
