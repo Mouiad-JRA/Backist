2ca001c4437209bcddfd20d9bb6478686ecf6d73
Mouiad JRA <mouiad.alilo@gmail.com>
2022-06-09 10:08:41 +0300

    fix: - the modal popup feat: - add the cookie modal

--- Banklist-site/script.js
+++ Banklist-site/script.js
@@ -7,8 +7,9 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
-
-const openModal = function () {
+const header = document.querySelector('.header');
+const openModal = function (event) {
+    event.preventDefault();
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
 };
@@ -17,9 +18,9 @@ const closeModal = function () {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
 };
-
-for (let i = 0; i < btnsOpenModal.length; i++)
-    btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach((btn)=>
+    btn.addEventListener('click', openModal)
+)
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +30,19 @@ document.addEventListener('keydown', function (e) {
         closeModal();
     }
 });
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.textContent = 'we use cookied for improved functionality and speed';
+message.innerHTML =
+    'we use cookied for improved functionality ' +
+    'and speed. <button class="btn ' +
+    'btn--close-cookie"> Got it!  </button>'
+
+ header.append(message);
+
+document.querySelector('.btn--close-cookie').addEventListener(
+    'click',()=>message.remove() // equal to message.parentElement.removeChild
+);
+
+// To get the style Style use getComputedStyle(element).property
